d19b23d9f239a40ca5c58b4d8d23c96017adb2c2
Fin Mavis <fin.mavis@yahoo.com>
2019-04-06 12:54:09 +0700

    Add initial config firebase and initial example to use firebase database

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from './firebase';
+
 class App extends Component {
+  componentDidMount() {
+    firebase
+      .database()
+      .ref('/tes')
+      .push({
+        name: 'My Name',
+        email: 'myemail@email.com',
+      });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React
           </a>
